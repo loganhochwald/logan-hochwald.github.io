57e6df4c3dad1fcca808e4eb4ee35db10f7554e1
Logan Hochwald <loganh42@gmail.com>
2023-01-09 18:53:16 +0000

    Almost Done with Scratchpad, Works in Greenlight

--- projects/scratch-pad/day-4/contact-list.js
+++ projects/scratch-pad/day-4/contact-list.js
@@ -61,46 +61,70 @@ function makeContactList(id, nameFirst, nameLast) {
      * You need something here to hold contacts. See length api for a hint:
      */
     var contacts = [];
+    //Empty array to store the contacts for the contact list
     
     return {
-        id: id,
-        nameFirst: nameFirst,
-        nameLast: nameLast,
+    //Return statement for function
+    id: id,
+    //Id key, stores what was put as the argument
+    nameFirst: nameFirst,
+    //First name key, stores what was put as the argument
+    nameLast: nameLast,
+    //Last name key, stores what was put as the argument
     
     length: function() 
+    //Length function that returns the length of the contact array
     {
-            return contacts.length;
+      return contacts.length;
     },
     
-    addContact: function (contact) 
+    addContact: function (contact)
+    //Add contact function that takes a new contact and pushes it into the contacts array 
     {
        return contacts.push(contact);
     
     },
-    findContact: function (fullName) 
+    findContact: function (fullName)
+    //Find contact function that takes fullName as the argument and matches with contact list
     {
       for (var i = 0; i <= contacts.length - 1; i++) {
+        //For loop runs through the contacts array until the entire length has been read
         if(fullName === contacts[i]["nameFirst"] + " " + contacts[i]["nameLast"]) {
+          //If the fullName argument matches the first name and last name of the contact, then it returns that contact name at the specific index observed
           return contacts[i];
         }
       }
     },
     removeContact: function (contact) 
+    //Remove contact function that takes contact as the parameter
     {
       for (var i = 0; i <= contacts.length - 1; i++) {
+        //For loop runs through the contacts array
         if (contacts[i] === contact) {
+          //If the entered contact matches a contact within the contacts array, then it is spliced
           return contacts.splice(i, 1);
+          //Splices the array at the defined index and 1, or the starting point
         }
       }
     },
-    printAllContactNames: function () 
+    printAllContactNames: function ()
+    //Prints all of the contact names function
     {
-        var stringReturn = "";
-
-        for (var i = 0; i <= contacts.length - 1; i++) {
-          stringReturn += contacts[i] + "/n";
+      var stringOfContacts = "";
+      //Creating empty string to store the name of the contacts within it
+      for (var i = 0; i < contacts.length; i++) {
+        //Runs through entire contacts array
+        if (i < contacts.length - 1) {
+        //If the counter has a length less than the last index of the array, add the index information and \n to stringOfContacts string
+        stringOfContacts += contacts[i] + "\n";
+        //Adding to string
+        } else {
+          //Once we've reached the end, only add the array information and not the \n this time so that there is no newline character at the end of the return list
+          stringOfContacts += contacts[i];
         }
-        return stringReturn;
+      }
+      return stringOfContacts;
+      //Returns the now filled stringOfContacts
     }
       
       };
