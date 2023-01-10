85f09827306f78698e4509ac663e7363a8ffe212
Logan Hochwald <loganh42@gmail.com>
2023-01-10 21:56:23 +0000

    Finished Matchy

--- projects/matchy/data.js
+++ projects/matchy/data.js
@@ -12,32 +12,46 @@
 // Step 1 - Object Creation //////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
 var animal = {};
+//Declaring empty animal object
 
 animal.species = "Sea Lion";
+//New key in object named species with data inserted
 animal["name"] = "Carl";
+//New key in object named name with data inserted
 animal.noises = [];
+//New key in object named noises which is an empty array
 console.log(animal);
+//Logging the animals object to the console
 
 //////////////////////////////////////////////////////////////////////
 // Step 2 - Array Creation ///////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
 
 var noises = [];
+//Creating empty array called noises
 noises[0] = 'bark';
+//At first position in array put "bark"
 noises.push('flop');
+//Now in last position is "flop" in array
 noises.unshift('arr');
+//Adding "arr" to beginning of array
 noises[noises.length] = 'sneeze';
+//Adding sneeze to the end of the array
 console.log(noises.length);
 console.log(noises[noises.length - 1]);
 console.log(noises);
+//Logging new data to console to confirm that it worked
 
 //////////////////////////////////////////////////////////////////////
 // Step 3 - Combining Step 1 and 2 ///////////////////////////////////
 //////////////////////////////////////////////////////////////////////
 
 animal['noises'] = noises;
-animal.noises.push('couch');
+//At the noises key in the animal object, inserting the previously created noises array
+animal.noises.push('cough');
+//Inserting "cough" to the end of the animal array
 console.log(animal);
+//Logging the animal array to the console
 
 /* *******************************************************************
  * Step 4 - Review
@@ -62,36 +76,46 @@ console.log(animal);
 // Step 6 - A Collection of Animals //////////////////////////////////
 //////////////////////////////////////////////////////////////////////
 var animals = [];
+//Declaring an empty array named animals
 
 animals.push(animal);
+//Adding the animal array to the end of the new animals array
 
 console.log(animals);
+//Logging the animals array to the console
 
 var duck = {
   species: "Duck",
   name: "Jerome",
   noises: ["quack", "honk", "sneeze", "whoosh"]
 };
+//Creating a new object named duck
 
 animals.push(duck);
+//Adding duck object to end of animals array
 
 console.log(animals);
+//Logging animals array to console
 
 var elephant = {
   species: "Elephant",
   name: "Ellie",
   noises: ["pheeeew", "sneeze"]
 };
+//Elephant is a new object
 
 var mockingBird = {
   species: "Mocking Bird",
   name: "Rock",
   noises: ["ahhhhhh", "burp"]
 };
+//Mockingbird is a new object
 
 animals.push(elephant, mockingBird);
+//Adding elephant and mockingbird object to end of the animals array
 
 console.log(animals, animals.length);
+//Logging animals array and the length to the console
 
 
 
@@ -101,20 +125,19 @@ console.log(animals, animals.length);
 var friends = [];
 //We want a list so chose array to store data
 
-function getRandom (array) {
-
-  return Math.random() * array.length;
-
+function getRandom (arr) {
+  return Math.floor(Math.random() * arr.length);
+  //Math.floor() is rounding the product of the length and Math.random() which returns a random number
 }
+friends.push(animals[getRandom(animals)].name);
+//Adding a random animals name by using a random index to the empty friends array
 
-friends.push(animals[getRandom(animals)]);
+console.log(friends)
+//Logging the friends array to the console
 
-console.log(getRandom(animals));
-console.log(friends);
-
-animals[elephant.friends] = friends;
-
-console.log(elephant);
+animals[2].friends = friends;
+//Friends list is being added as a property to the animal object at index 2 in the animals array
+//The above sentence makes sense if you read it slowly, you got this.
 
 /**
  * Nice work! You're done Part 1. Pat yourself on the back and
--- projects/matchy/functions.js
+++ projects/matchy/functions.js
@@ -12,25 +12,69 @@
 //////////////////////////////////////////////////////////////////////
 // Step 1 - Search ///////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
+function search(arr, str){
 
+    for(i = 0; i < arr.length; i++){
+    //For loop iterating through array until end
+        if(arr[i]['name'] === str){
+        //If the array's object at that index has a name that is the same as the inputted string
+            return arr[i];
+            //Return the animal object
+        }
+    } 
+    return null;
+    //Otherwise return null
+}
 
 
 //////////////////////////////////////////////////////////////////////
 // Step 2 - Replace //////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
-
+function replace(animals, name, replacement){
+    for(let i in animals){
+        //Iterating through the entire array
+        if(animals[i]['name'] === name){
+            //If the array's object at that index has the same name property as the string
+            animals[i] = replacement;
+            //Replace the object with the new inputted object
+        }
+    }
+    return;
+}
 
 
 //////////////////////////////////////////////////////////////////////
 // Step 3 - Remove ///////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
-
+function remove(animals, name){
+    for(let i in animals){
+        //Iterating through entire array
+        if(animals[i]['name'] === name){
+            //If the object at that array index has same name property as inputted string
+            animals.splice(animals[i], 1);
+            //Remove the name at that index and remove only one "thing"
+        }
+    }
+}
 
 
 //////////////////////////////////////////////////////////////////////
 // Step 4 - Add ///////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
-
+function add(animals, animal){
+    for(let i in animals){
+        //Iterating through entire array
+        if(animals[i]['name'] !== animal.name){
+            //If the object at that array index does not have the same name property as animal
+            if(animal.name.length > 0 && animal.species.length > 0){
+                //If animal isn't empty
+                animals.push(animal);
+                //Insert the animal into the animals array
+            }
+        } else {return;}
+        //Otherwise return
+    }
+}
 
 
 /**
