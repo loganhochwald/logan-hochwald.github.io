6f7aa9b00acf3f16d7e3628a22a9de9c22b79b48
Logan Hochwald <loganh42@gmail.com>
2023-02-03 07:52:54 +0000

    Finished Variables Only Functions Left

--- studies/variables.js
+++ studies/variables.js
@@ -1,5 +1,5 @@
 /*
-   * VARIABLES: ****CHANGE HOISTING AND ADD AN EXAMPLE****
+   * VARIABLES:
    *
    * 0. To hold things in memory during the life-cycle of a program, we can use variables.  Variables
    * are named identifiers that can point to values of a particular type, like a Number, String,
@@ -11,7 +11,13 @@
    *
    * 2. There are 2 phases of using variables: declaration and initialization (or assignment).
    * 
-   * 3. Hoisting is a process that
+   * 3. Hoisting is a process that the Javascript interpreter does where it hoists the declarations, assignment of variables,
+   * and assignment of functions to the top of the scope before code execution. This means that the interpreter scans the code
+   * and hoists variable and function declarations to the top of the code, and then the code is executed. Function declarations
+   * and their definitions take precedence over var declarations. It is important to note that initializations are not hoisted, 
+   * so if the value of a var variable is called before it had been declared, then it would resolve to an undefined error. Similarly,
+   * the let and const variables can't be accessed until they're defined but a reference error is thrown because they are not 
+   * initialized as undefined.
    */
 
   // 1. declaration //
@@ -35,3 +41,21 @@
   var myVariable = 1;
   var myVariable = true;
   myVariable = "someString";
+
+  // 3. hoisting //
+
+  //Written Code:
+  var numberOfCows = 8;
+  var howManyCows = getCowNumber(numberOfCows);
+  function getCowNumber(cow) {
+   return "This many cows: " + cow;
+  }
+
+  //Javascript interprets like this:
+  function getCowNumber(cow) {
+   return "This many cows: " + cow;
+  }
+  var numberOfCows;
+  var howManyCows
+  numberOfCows = 8;
+  howManyCows = getCowNumber(numberOfCows);
